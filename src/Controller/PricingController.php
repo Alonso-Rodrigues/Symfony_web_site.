32a6b824d3a0eb9e00c6cf23b796c6fa9e7fe306
Alonso <alonso.rbass@gmail.com>
2023-09-13 14:23:50 +0200

    ok

--- src/Controller/PricingController.php
+++ src/Controller/PricingController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\PricingPlan;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +11,13 @@ class PricingController extends AbstractController
     #[Route('/pricing', name: 'app_pricing')]
     public function index(): Response
     {
+        $pricingPlans = $this->getDoctrine() //to connect with the BD
+            ->getRepository(PricingPlan::class)
+            ->findAll();
+
         return $this->render('pricing/index.html.twig', [
-            'controller_name' => 'PricingController',
+            'pricingPlans' => $pricingPlans,
         ]);
     }
+
 }
